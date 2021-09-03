017bf55f0b3f7a1c7da3e5f9c68d823244f8b4d6
jeffersonvega16 <jivega@fiec.espol.edu.ec>
2021-09-02 21:46:16 -0500

    Cambios en files backend

--- backend/src/controller/poblacion.controller.js
+++ backend/src/controller/poblacion.controller.js
@@ -1,7 +1,7 @@
 //const connect = dbConnection();
 const { dbConnection } = require('../config/database');
 let getPoblacion = async(req,res)=>{
-    await connection.query("query 1********",(err,result)=>{
+    await connection.query("select pais.nombre, idPoblacion, TotalCases  from poblacion inner join pais  on poblacion.idPoblacion= pais.continente_idcontinente order by TotalCases desc limit 5",(err,result)=>{
         if(result){
             res.send(result);
         }else{
